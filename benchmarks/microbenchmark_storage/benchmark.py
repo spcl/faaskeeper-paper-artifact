829d07ca1d11cd89b5fc78c31fd0db15b1ea96cd
Marcin Copik <mcopik@gmail.com>
2021-09-08 20:47:52 +0200

    Code linting

--- benchmarks/microbenchmark_storage/benchmark.py
+++ benchmarks/microbenchmark_storage/benchmark.py
@@ -1,4 +1,3 @@
-
 import base64
 import sys
 import time
@@ -6,20 +5,20 @@ from datetime import datetime, timedelta
 
 import pandas as pd
 import boto3
-from boto3.dynamodb.types import Binary
 
 
 region = sys.argv[1]
 output_prefix = sys.argv[2]
 repetitions = int(sys.argv[3])
 storage_type = sys.argv[4]
-cleanup = sys.argv[5].lower() == 'true'
+cleanup = sys.argv[5].lower() == "true"
 max_size = int(sys.argv[6]) if len(sys.argv) == 7 else -1
 
 # in bytes
 # base64 encoded string begins at 4 bytes anyway
-BENCHMARK_SIZES = [2**i for i in range(2, 20)]
+BENCHMARK_SIZES = [2 ** i for i in range(2, 20)]
 DYNAMODB_TABLE_NAME = "faaskeeper_microbenchmark_storage"
+
 
 def generate_binary_data(size):
     # for n bytes the length of base64 string is 4 * n / 3 (unpadded)
@@ -28,11 +27,14 @@ def generate_binary_data(size):
     original_size = int(size * 3 / 4)
     return base64.b64encode(bytearray([1] * original_size))
 
+
 def test_s3_read():
     pass
 
+
 def test_s3_write():
     pass
+
 
 def test_dynamodb_read(client, name, size, repetitions):
 
@@ -42,19 +44,16 @@ def test_dynamodb_read(client, name, size, repetitions):
         begin = datetime.now()
         ret = client.get_item(
             TableName=DYNAMODB_TABLE_NAME,
-            Key={
-                "key": {
-                    'S': name
-                },
-            },
-            ReturnConsumedCapacity='TOTAL',
-            ConsistentRead=True
+            Key={"key": {"S": name}},
+            ReturnConsumedCapacity="TOTAL",
+            ConsistentRead=True,
         )
-        read_capacity += ret['ConsumedCapacity']['CapacityUnits']
+        read_capacity += ret["ConsumedCapacity"]["CapacityUnits"]
         end = datetime.now()
         results.append(int((end - begin) / timedelta(microseconds=1)))
     print(f"Expected read size {size}, output data size: {len(ret['Item']['data']['B'])}")
     return results, read_capacity
+
 
 def test_dynamodb_write(client, name, size, repetitions):
 
@@ -66,17 +65,10 @@ def test_dynamodb_write(client, name, size, repetitions):
         begin = datetime.now()
         ret = client.put_item(
             TableName=DYNAMODB_TABLE_NAME,
-            Item={
-                "key": {
-                    'S': name
-                },
-                "data": {
-                    'B': data
-                }
-            },
-            ReturnConsumedCapacity='TOTAL'
+            Item={"key": {"S": name}, "data": {"B": data}},
+            ReturnConsumedCapacity="TOTAL",
         )
-        write_capacity += ret['ConsumedCapacity']['CapacityUnits']
+        write_capacity += ret["ConsumedCapacity"]["CapacityUnits"]
         end = datetime.now()
         results.append(int((end - begin) / timedelta(microseconds=1)))
     return results, write_capacity
@@ -85,29 +77,21 @@ def test_dynamodb_write(client, name, size, repetitions):
 def init_s3():
     pass
 
+
 def delete_s3(client):
     pass
+
 
 def init_dynamodb(region):
     client = boto3.client("dynamodb")
 
     # Testing table: string key, binary data
     try:
-        ret = client.create_table(
-            AttributeDefinitions=[
-                {
-                'AttributeName': 'key',
-                'AttributeType': 'S'
-                }
-            ],
+        client.create_table(
+            AttributeDefinitions=[{"AttributeName": "key", "AttributeType": "S"}],
             TableName=DYNAMODB_TABLE_NAME,
-            KeySchema=[
-                {
-                    'AttributeName': 'key',
-                    'KeyType': 'HASH'
-                },
-            ],
-            BillingMode='PAY_PER_REQUEST'
+            KeySchema=[{"AttributeName": "key", "KeyType": "HASH"}],
+            BillingMode="PAY_PER_REQUEST",
         )
         print("Sleep to make sure the table is up and running")
         time.sleep(5)
@@ -119,10 +103,11 @@ def init_dynamodb(region):
 def delete_dynamodb(client):
     client.delete_table(TableName=DYNAMODB_TABLE_NAME)
 
+
 print(f"Initialize {storage_type}")
-if storage_type == 's3':
+if storage_type == "s3":
     client = init_s3()
-elif storage_type == 'dynamodb':
+elif storage_type == "dynamodb":
     client = init_dynamodb(region)
 else:
     raise NotImplementedError()
@@ -136,34 +121,33 @@ for size in BENCHMARK_SIZES:
     print(f"Testing write data of size {size}")
     results, write_capacity = test_dynamodb_write(client, f"size_{size}", size, repetitions)
     df_write = pd.DataFrame(data=results, columns=["data"])
-    df_write['op'] = 'write'
-    df_write = df_write.append({'data': write_capacity, 'op': 'write_capacity'}, ignore_index=True)
-    df_write['storage'] = 'dynamodb'
-    df_write['size'] = size
+    df_write["op"] = "write"
+    df_write = df_write.append({"data": write_capacity, "op": "write_capacity"}, ignore_index=True)
+    df_write["storage"] = "dynamodb"
+    df_write["size"] = size
     dfs.append(df_write)
 
     print(f"Testing read data of size {size}")
     results, read_capacity = test_dynamodb_read(client, f"size_{size}", size, repetitions)
     df_read = pd.DataFrame(data=results, columns=["data"])
-    df_read['op'] = 'read'
-    df_read['storage'] = 'dynamodb'
-    df_read = df_read.append({'data': read_capacity, 'op': 'read_capacity'}, ignore_index=True)
-    df_read['size'] = size
+    df_read["op"] = "read"
+    df_read["storage"] = "dynamodb"
+    df_read = df_read.append({"data": read_capacity, "op": "read_capacity"}, ignore_index=True)
+    df_read["size"] = size
     dfs.append(df_read)
 
     if max_size != -1 and size > max_size:
         break
     break
 
-df = pd.concat(dfs, axis=0, ignore_index = True)
+df = pd.concat(dfs, axis=0, ignore_index=True)
 df.to_csv(f"{output_prefix}.csv")
 
 if cleanup:
     print(f"Clean {storage_type}")
-    if storage_type == 's3':
+    if storage_type == "s3":
         delete_s3(client)
-    elif storage_type == 'dynamodb':
+    elif storage_type == "dynamodb":
         delete_dynamodb(client)
     else:
         raise NotImplementedError()
-
