d76dc08cb3eb00abab2deb325ed7fd7b5f7b95a4
Marcin Copik <mcopik@gmail.com>
2021-09-08 18:48:53 +0200

    Add installation of depds

---
+++ install.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+import subprocess
+
+parser = argparse.ArgumentParser(description="Install FK artifact and dependencies.")
+parser.add_argument('--venv', metavar='dir', type=str, default="python-venv", help='destination of local python and nodejs virtual environment')
+parser.add_argument('--without-client-library', action='store_true', default=False, help='skip installation of client library')
+args = parser.parse_args()
+
+def execute(cmd):
+    ret = subprocess.run(
+        cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True
+    )
+    if ret.returncode:
+        raise RuntimeError(
+            "Running {} failed!\n Output: {}".format(cmd, ret.stdout.decode("utf-8"))
+        )
+    return ret.stdout.decode("utf-8")
+
+python_env_dir = args.venv
+
+print("Creating Python virtualenv at {}".format(python_env_dir))
+execute("python3 -mvenv {}".format(python_env_dir))
+
+print("Install Python dependencies with pip")
+execute(". {}/bin/activate && pip3 install -r requirements.txt".format(python_env_dir))
+
+if not args.without_client_library:
+    print("Install FaaSKeeper Python library")
+    execute(". {}/bin/activate && pip install git+ssh://git@github.com/mcopik/faaskeeper-python.git --upgrade".format(python_env_dir))
+
