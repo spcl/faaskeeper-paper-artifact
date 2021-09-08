20a330a7a94e4be06b3876907c52207f38b90723
Marcin Copik <mcopik@gmail.com>
2021-09-08 23:18:41 +0000

    Update benchmark code

--- benchmarks/microbenchmark_storage/benchmark.py
+++ benchmarks/microbenchmark_storage/benchmark.py
@@ -1,3 +1,4 @@
+import argparse
 import base64
 import sys
 import time
@@ -6,19 +7,24 @@ from datetime import datetime, timedelta
 import pandas as pd
 import boto3
 
-
-region = sys.argv[1]
-output_prefix = sys.argv[2]
-repetitions = int(sys.argv[3])
-storage_type = sys.argv[4]
-cleanup = sys.argv[5].lower() == "true"
-max_size = int(sys.argv[6]) if len(sys.argv) == 7 else -1
+parser = argparse.ArgumentParser(description="Run microbenchmarks.")
+parser.add_argument('--region', type=str)
+parser.add_argument('--output-prefix', type=str)
+parser.add_argument('--repetitions', type=int)
+parser.add_argument('--storage-type', type=str)
+parser.add_argument('--cleanup', action='store_true', default=False)
+parser.add_argument('--max-size', type=int, default=-1)
+parser.add_argument('--storage-suffix', type=str, default="")
+args = parser.parse_args()
 
 # in bytes
 # base64 encoded string begins at 4 bytes anyway
 BENCHMARK_SIZES = [2 ** i for i in range(2, 20)]
 DYNAMODB_TABLE_NAME = "faaskeeper_microbenchmark_storage"
-S3_BUCKET_NAME = "faaskeeper-microbenchmark-storage-inter"
+S3_BUCKET_NAME = "faaskeeper-microbenchmark-storage"
+if args.storage_suffix != "":
+    S3_BUCKET_NAME += f"-{args.storage_suffix}"
+
 
 
 def generate_binary_data(size):
@@ -140,11 +146,11 @@ def delete_dynamodb(client):
     client.delete_table(TableName=DYNAMODB_TABLE_NAME)
 
 
-print(f"Initialize {storage_type}")
-if storage_type == "s3":
-    client = init_s3(region)
-elif storage_type == "dynamodb":
-    client = init_dynamodb(region)
+print(f"Initialize {args.storage_type}")
+if args.storage_type == "s3":
+    client = init_s3(args.region)
+elif args.storage_type == "dynamodb":
+    client = init_dynamodb(args.region)
 else:
     raise NotImplementedError()
 
@@ -152,13 +158,13 @@ dfs = []
 for size in BENCHMARK_SIZES:
 
     # evaluate at the maximum
-    if max_size != -1 and size > max_size:
-        size = max_size
+    if args.max_size != -1 and size > args.max_size:
+        size = args.max_size
     print(f"Testing write data of size {size}")
-    if storage_type == "s3":
-        results, write_capacity = test_s3_write(client, f"size_{size}", size, repetitions)
-    elif storage_type == "dynamodb":
-        results, write_capacity = test_dynamodb_write(client, f"size_{size}", size, repetitions)
+    if args.storage_type == "s3":
+        results, write_capacity = test_s3_write(client, f"size_{size}", size, args.repetitions)
+    elif args.storage_type == "dynamodb":
+        results, write_capacity = test_dynamodb_write(client, f"size_{size}", size, args.repetitions)
     else:
         raise NotImplementedError()
     df_write = pd.DataFrame(data=results, columns=["data"])
@@ -169,31 +175,30 @@ for size in BENCHMARK_SIZES:
     dfs.append(df_write)
 
     print(f"Testing read data of size {size}")
-    if storage_type == "s3":
-        results, read_capacity = test_s3_read(client, f"size_{size}", size, repetitions)
-    elif storage_type == "dynamodb":
-        results, read_capacity = test_dynamodb_read(client, f"size_{size}", size, repetitions)
+    if args.storage_type == "s3":
+        results, read_capacity = test_s3_read(client, f"size_{size}", size, args.repetitions)
+    elif args.storage_type == "dynamodb":
+        results, read_capacity = test_dynamodb_read(client, f"size_{size}", size, args.repetitions)
     else:
         raise NotImplementedError()
     df_read = pd.DataFrame(data=results, columns=["data"])
     df_read["op"] = "read"
-    df_read["storage"] = "dynamodb"
     df_read = df_read.append({"data": read_capacity, "op": "read_capacity"}, ignore_index=True)
+    df_read["storage"] = "dynamodb"
     df_read["size"] = size
     dfs.append(df_read)
 
-    if max_size != -1 and size > max_size:
+    if args.max_size != -1 and size > args.max_size:
         break
-    break
 
 df = pd.concat(dfs, axis=0, ignore_index=True)
-df.to_csv(f"{output_prefix}.csv")
+df.to_csv(f"{args.output_prefix}.csv")
 
-if cleanup:
-    print(f"Clean {storage_type}")
-    if storage_type == "s3":
+if args.cleanup:
+    print(f"Clean {args.storage_type}")
+    if args.storage_type == "s3":
         delete_s3(client)
-    elif storage_type == "dynamodb":
+    elif args.storage_type == "dynamodb":
         delete_dynamodb(client)
     else:
         raise NotImplementedError()
