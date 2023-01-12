06a4d40be7cba31a04259adf8e1e7fcd0eabd2dd
Marcin Copik <mcopik@gmail.com>
2023-01-12 16:09:56 +0000

    Add max size to benchmarker

--- benchmarks/zookeeper_read/run.sh
+++ benchmarks/zookeeper_read/run.sh
@@ -3,6 +3,6 @@
 PORT=60000
 REPETITIONS=1000
 
-python3 benchmark.py --config test_read.json --port $PORT --repetitions $REPETITIONS --output-prefix ../../data/read/key_value
+python3 benchmark.py --config test_read.json --port $PORT--repetitions $REPETITIONS --max-size 408576 --output-prefix ../../data/read/key_value
 # FIXME: reconfigure functions
 #python3 benchmark.py --config test_read.json --port $PORT --repetitions $REPETITIONS --output-prefix ../../data/read/persistent
